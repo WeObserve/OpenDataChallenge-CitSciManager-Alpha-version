e8ce03acea8aabc052b45768566ed123a62f2155
shiva <anindyapandey@gmail.com>
2020-06-28 14:19:08 +0530

    remove datastories related controllers

--- backend/server.py
+++ backend/server.py
@@ -3,7 +3,6 @@ from flask import request
 import json
 from file_service import process_upload_file, process_download_file
 from user_service import process_invite_users
-from datastory_service import process_create_datastory, process_view_datastory
 from common_service import validate_request
 from db.mongo import mongo_connection
 
@@ -81,56 +80,6 @@ def invite_users():
         return json.dumps(response)
 
     process_response = process_invite_users(mongo_db_connection, request, request_validity["user"], env)
-
-    if process_response["is_success"]:
-        response["success_response"] = process_response
-    else:
-        response["error_response"] = process_response
-
-    return response
-
-@app.route('/datastories', methods=["POST"])
-def create_datastory():
-    response = {
-        "success_response": None,
-        "error_response": None
-    }
-
-    request_validity = validate_request(request, "CREATE_DATASTORY", "form")
-
-    if (not request_validity["is_valid"]):
-        response["error_response"] = {
-            "request_validity": request_validity
-        }
-
-        return json.dumps(response)
-
-    process_response = process_create_datastory(request, request_validity["user"])
-
-    if process_response["is_success"]:
-        response["success_response"] = process_response
-    else:
-        response["error_response"] = process_response
-
-    return response
-
-@app.route('/datastories/view', methods=["POST"])
-def view_datastory():
-    response = {
-        "success_response": None,
-        "error_response": None
-    }
-
-    request_validity = validate_request(request, "VIEW_DATASTORIES", "json")
-
-    if (not request_validity["is_valid"]):
-        response["error_response"] = {
-            "request_validity": request_validity
-        }
-
-        return json.dumps(response)
-
-    process_response = process_view_datastory(request, request_validity["user"])
 
     if process_response["is_success"]:
         response["success_response"] = process_response
