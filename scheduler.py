6816bc997514176ffcc2d5d6b92a5934adb87119
shiva <anindyapandey@gmail.com>
2020-07-26 23:50:24 +0530

    exception logging

--- scheduler.py
+++ scheduler.py
@@ -64,147 +64,151 @@ def send_join_done_email(created_user, env, s3_link, email_sender_address):
     )
 
 def process_uploaded_files():
-    log_to_file("Inside process_uploaded_files\n")
+    try:
+        log_to_file("Inside process_uploaded_files\n")
 
-    processing_files_cursor = files_dao.get_files(mongo_db_connection, {"status": "PROCESSING"})
+        processing_files_cursor = files_dao.get_files(mongo_db_connection, {"status": "PROCESSING"})
 
-    if processing_files_cursor is not None and processing_files_cursor.count() != 0:
-        log_to_file("There are some files still being processed hence skipping\n")
-        return
-
-    processing_joins_cursor = joins_dao.get_joins(mongo_db_connection, {"status": "PROCESSING"})
-
-    if processing_joins_cursor is not None and processing_joins_cursor.count() != 0:
-        log_to_file("There are some joins still being processed hence skipping\n")
-        return
-
-    uploaded_files_cursor = files_dao.get_files(mongo_db_connection, {"file_type": "META_DATA", "status": "UPLOADED"})
-    pending_joins_cursor = joins_dao.get_joins(mongo_db_connection, {"status": "PENDING"})
-
-    if (uploaded_files_cursor is None or uploaded_files_cursor.count() == 0) and (pending_joins_cursor is None or pending_joins_cursor.count() == 0):
-        log_to_file("There are no new meta data files uploaded and no pending joins hence skipping\n")
-        return
-
-    if uploaded_files_cursor is not None and uploaded_files_cursor.count() != 0:
-        uploaded_file_to_process = uploaded_files_cursor[0]
-
-        files_dao.update_file(mongo_db_connection, {"_id": uploaded_file_to_process["_id"]}, {"$set": {"status": "PROCESSING"}})
-
-        bucket_name = uploaded_file_to_process["s3_link"][8:].split('.')[0]
-
-        url = 's3n://' + bucket_name + "/" + uploaded_file_to_process["relative_path"]
-
-        if uploaded_file_to_process["relative_path"][0] == "/":
-            url = 's3n://' + bucket_name + uploaded_file_to_process["relative_path"]
-
-        log_to_file(url + "\n")
-
-        df = spark.read.csv(url, header=True).limit(1)
-
-        files_dao.update_file(mongo_db_connection, {"_id": uploaded_file_to_process["_id"]},
-                              {"$set": {"status": "PROCESSED", "headers": df.first().__fields__}})
-
-        log_to_file("uploaded file processing done\n")
-    else:
-        pending_join_to_process = pending_joins_cursor[0]
-
-        joins_dao.update_join(mongo_db_connection, {"_id": pending_join_to_process["_id"]}, {"$set": {"status": "PROCESSING"}})
-
-        files_to_be_joined_cursor = files_dao.get_files(mongo_db_connection, {
-            "_id": {
-                "$in": [pending_join_to_process["file_id_1"], pending_join_to_process["file_id_2"]]
-            },
-            "status": "PROCESSED",
-            "file_type": "META_DATA"
-        })
-
-        if files_to_be_joined_cursor is None or files_to_be_joined_cursor.count() != 2:
-            log_to_file("No files found for joining hence skipping\n")
-            joins_dao.update_join(mongo_db_connection, {"_id": pending_join_to_process["_id"]},
-                                  {"$set": {"status": "PROCESSED"}})
+        if processing_files_cursor is not None and processing_files_cursor.count() != 0:
+            log_to_file("There are some files still being processed hence skipping\n")
             return
 
-        file1 = None
-        file2 = None
+        processing_joins_cursor = joins_dao.get_joins(mongo_db_connection, {"status": "PROCESSING"})
 
-        for file in files_to_be_joined_cursor:
-            if str(file["_id"]) == str(pending_join_to_process["file_id_1"]):
-                file1 = file
-                continue
-            if str(file["_id"]) == str(pending_join_to_process["file_id_2"]):
-                file2 = file
+        if processing_joins_cursor is not None and processing_joins_cursor.count() != 0:
+            log_to_file("There are some joins still being processed hence skipping\n")
+            return
 
-        bucket_name_for_file_1 = file1["s3_link"][8:].split('.')[0]
-        bucket_name_for_file_2 = file2["s3_link"][8:].split('.')[0]
+        uploaded_files_cursor = files_dao.get_files(mongo_db_connection, {"file_type": "META_DATA", "status": "UPLOADED"})
+        pending_joins_cursor = joins_dao.get_joins(mongo_db_connection, {"status": "PENDING"})
 
-        url_for_file_1 = 's3n://' + bucket_name_for_file_1 + "/" + file1["relative_path"]
-        url_for_file_2 = 's3n://' + bucket_name_for_file_2 + "/" + file2["relative_path"]
+        if (uploaded_files_cursor is None or uploaded_files_cursor.count() == 0) and (pending_joins_cursor is None or pending_joins_cursor.count() == 0):
+            log_to_file("There are no new meta data files uploaded and no pending joins hence skipping\n")
+            return
 
-        if file1["relative_path"][0] == "/":
-            url = 's3n://' + bucket_name_for_file_1 + file1["relative_path"]
+        if uploaded_files_cursor is not None and uploaded_files_cursor.count() != 0:
+            uploaded_file_to_process = uploaded_files_cursor[0]
 
-        if file2["relative_path"][0] == "/":
-            url = 's3n://' + bucket_name_for_file_2 + file2["relative_path"]
+            files_dao.update_file(mongo_db_connection, {"_id": uploaded_file_to_process["_id"]}, {"$set": {"status": "PROCESSING"}})
 
-        log_to_file(url_for_file_1 + "\n")
-        log_to_file(url_for_file_2 + "\n")
+            bucket_name = uploaded_file_to_process["s3_link"][8:].split('.')[0]
 
-        df1 = spark.read.csv(url_for_file_1, header=True)
-        df2 = spark.read.csv(url_for_file_2, header=True)
+            url = 's3n://' + bucket_name + "/" + uploaded_file_to_process["relative_path"]
 
-        df1 = df1.select(pending_join_to_process["columns_for_file_1"])
-        df2 = df2.select(pending_join_to_process["columns_for_file_2"])
+            if uploaded_file_to_process["relative_path"][0] == "/":
+                url = 's3n://' + bucket_name + uploaded_file_to_process["relative_path"]
 
-        new_column_names_for_df2 = []
-        for column in df2.columns:
-            new_column_names_for_df2.append('b' + column)
+            log_to_file(url + "\n")
 
-        df2 = df2.toDF(*new_column_names_for_df2)
+            df = spark.read.csv(url, header=True).limit(1)
 
-        df = df1.join(df2, df1[pending_join_to_process["join_column_for_file_1"]] == df2['b' + pending_join_to_process["join_column_for_file_2"]], how="inner")
+            files_dao.update_file(mongo_db_connection, {"_id": uploaded_file_to_process["_id"]},
+                                  {"$set": {"status": "PROCESSED", "headers": df.first().__fields__}})
 
-        columns = []
-        rep_count = 0
-        for column in df.columns:
-            if column in columns:
-                df = df.withColumnRenamed(column, str(rep_count) + column)
-                columns.append(str(rep_count) + column)
-                continue
-            columns.append(column)
+            log_to_file("uploaded file processing done\n")
+        else:
+            pending_join_to_process = pending_joins_cursor[0]
 
-        df.show()
+            joins_dao.update_join(mongo_db_connection, {"_id": pending_join_to_process["_id"]}, {"$set": {"status": "PROCESSING"}})
 
-        log_to_file("join done...trying to upload to s3\n")
+            files_to_be_joined_cursor = files_dao.get_files(mongo_db_connection, {
+                "_id": {
+                    "$in": [pending_join_to_process["file_id_1"], pending_join_to_process["file_id_2"]]
+                },
+                "status": "PROCESSED",
+                "file_type": "META_DATA"
+            })
 
-        pd_df = df.toPandas()
+            if files_to_be_joined_cursor is None or files_to_be_joined_cursor.count() != 2:
+                log_to_file("No files found for joining hence skipping\n")
+                joins_dao.update_join(mongo_db_connection, {"_id": pending_join_to_process["_id"]},
+                                      {"$set": {"status": "PROCESSED"}})
+                return
 
-        bytes_to_write = pd_df.to_csv(None, index=False).encode()
+            file1 = None
+            file2 = None
 
-        fs = s3fs.S3FileSystem(key=config[env].access_key_id, secret=config[env].secret_access_key)
+            for file in files_to_be_joined_cursor:
+                if str(file["_id"]) == str(pending_join_to_process["file_id_1"]):
+                    file1 = file
+                    continue
+                if str(file["_id"]) == str(pending_join_to_process["file_id_2"]):
+                    file2 = file
 
-        with fs.open("s3://" + bucket_name_for_file_1 + "/joins/" + str(pending_join_to_process["_id"]) + ".csv", 'wb') as f:
-            f.write(bytes_to_write)
+            bucket_name_for_file_1 = file1["s3_link"][8:].split('.')[0]
+            bucket_name_for_file_2 = file2["s3_link"][8:].split('.')[0]
 
-        file_dict = {
-            "file_name": str(pending_join_to_process["_id"]) + ".csv",
-            "s3_link": "https://" + bucket_name_for_file_1 + ".s3-us-west-2.amazonaws.com/joins/" + str(pending_join_to_process["_id"]) + ".csv",
-            "relative_path": "joins/" + str(pending_join_to_process["_id"]) + ".csv",
-            "project_id": pending_join_to_process["project_id"],
-            "creator_id": pending_join_to_process["user_id"],
-            "status": "PROCESSED",
-            "headers": df.columns,
-            "file_type": "META_DATA"
-        }
+            url_for_file_1 = 's3n://' + bucket_name_for_file_1 + "/" + file1["relative_path"]
+            url_for_file_2 = 's3n://' + bucket_name_for_file_2 + "/" + file2["relative_path"]
 
-        files_dao.insert_file(mongo_db_connection, file_dict)
+            if file1["relative_path"][0] == "/":
+                url = 's3n://' + bucket_name_for_file_1 + file1["relative_path"]
 
-        user = users_dao.get_users(mongo_db_connection, {"_id": pending_join_to_process["user_id"]})[0]
+            if file2["relative_path"][0] == "/":
+                url = 's3n://' + bucket_name_for_file_2 + file2["relative_path"]
 
-        send_join_done_email(User(user), env, file_dict["s3_link"], config[env].email_sender_address)
+            log_to_file(url_for_file_1 + "\n")
+            log_to_file(url_for_file_2 + "\n")
 
-        joins_dao.update_join(mongo_db_connection, {"_id": pending_join_to_process["_id"]}, {"$set": {"status": "PROCESSED"}})
+            df1 = spark.read.csv(url_for_file_1, header=True)
+            df2 = spark.read.csv(url_for_file_2, header=True)
 
-        log_to_file("pending join processing done\n")
+            df1 = df1.select(pending_join_to_process["columns_for_file_1"])
+            df2 = df2.select(pending_join_to_process["columns_for_file_2"])
+
+            new_column_names_for_df2 = []
+            for column in df2.columns:
+                new_column_names_for_df2.append('b' + column)
+
+            df2 = df2.toDF(*new_column_names_for_df2)
+
+            df = df1.join(df2, df1[pending_join_to_process["join_column_for_file_1"]] == df2['b' + pending_join_to_process["join_column_for_file_2"]], how="inner")
+
+            columns = []
+            rep_count = 0
+            for column in df.columns:
+                if column in columns:
+                    df = df.withColumnRenamed(column, str(rep_count) + column)
+                    columns.append(str(rep_count) + column)
+                    continue
+                columns.append(column)
+
+            df.show()
+
+            log_to_file("join done...trying to upload to s3\n")
+
+            pd_df = df.toPandas()
+
+            bytes_to_write = pd_df.to_csv(None, index=False).encode()
+
+            fs = s3fs.S3FileSystem(key=config[env].access_key_id, secret=config[env].secret_access_key)
+
+            with fs.open("s3://" + bucket_name_for_file_1 + "/joins/" + str(pending_join_to_process["_id"]) + ".csv", 'wb') as f:
+                f.write(bytes_to_write)
+
+            file_dict = {
+                "file_name": str(pending_join_to_process["_id"]) + ".csv",
+                "s3_link": "https://" + bucket_name_for_file_1 + ".s3-us-west-2.amazonaws.com/joins/" + str(pending_join_to_process["_id"]) + ".csv",
+                "relative_path": "joins/" + str(pending_join_to_process["_id"]) + ".csv",
+                "project_id": pending_join_to_process["project_id"],
+                "creator_id": pending_join_to_process["user_id"],
+                "status": "PROCESSED",
+                "headers": df.columns,
+                "file_type": "META_DATA"
+            }
+
+            files_dao.insert_file(mongo_db_connection, file_dict)
+
+            user = users_dao.get_users(mongo_db_connection, {"_id": pending_join_to_process["user_id"]})[0]
+
+            send_join_done_email(User(user), env, file_dict["s3_link"], config[env].email_sender_address)
+
+            joins_dao.update_join(mongo_db_connection, {"_id": pending_join_to_process["_id"]}, {"$set": {"status": "PROCESSED"}})
+
+            log_to_file("pending join processing done\n")
+
+    except Exception as e:
+        log_to_file(str(e))
 
 if __name__ == '__main__':
     log_to_file("\nI reached this")
